1aea68953c5fb40e9c7b6619f74fb7add776f345
Fernando Nogueira <fernandojnn@gmail.com>
2018-01-26 18:34:06 -0200

    Improving

--- src/main/java/ox/Configuration.java
+++ src/main/java/ox/Configuration.java
@@ -2,12 +2,7 @@ package ox;
 
 public final class Configuration {
 
-    /**
-     * Utils
-     */
-    private Configuration() {
-    }
-
     public static final String MIGRATION_COLLECTION_VERSION_ATTRIBUTE = "version";
     public static final String SCHEMA_VERSION_COLLECTION_NAME = "migration_versions";
+
 }
