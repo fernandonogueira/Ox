e8f666d06383ce6c47a80fa41fc1960ef027ec78
Fernando Nogueira <fernandojnn@gmail.com>
2018-02-04 20:53:08 -0200

    Fixing broken docs (because of the previous format commit)

--- docs/conf.py
+++ docs/conf.py
@@ -50,7 +50,7 @@ source_parsers = {
 # You can specify multiple suffix as a list of string:
 #
 # source_suffix = ['.rst', '.md']
-# source_suffix = '.rst'
+#source_suffix = '.rst'
 source_suffix = ['.rst', '.md']
 
 # The master toctree document.
@@ -93,7 +93,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# html_theme = 'alabaster'
+#html_theme = 'alabaster'
 html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
