b4a6761b501ca56431f00193a7501d06985119d6
Fernando Nogueira <fernandonogueira@users.noreply.github.com>
2024-03-04 02:29:27 -0300

    chore: add create ttl index test

---
+++ src/test/java/ox/integration/ttl/CreateTTLIndexTest.java
@@ -0,0 +1,41 @@
+package ox.integration.ttl;
+
+import org.assertj.core.api.Assertions;
+import org.bson.Document;
+import org.junit.Test;
+import ox.engine.Ox;
+import ox.engine.OxConfig;
+import ox.engine.exception.InvalidMongoConfiguration;
+import ox.integration.base.OxBaseContainerTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CreateTTLIndexTest extends OxBaseContainerTest {
+
+    @Test
+    public void shouldCreateTTLIndex() throws InvalidMongoConfiguration {
+
+        OxConfig config = OxConfig.builder()
+                .mongo(getDefaultMongo())
+                .databaseName("create-ttl-index-test")
+                .scanPackage("ox.integration.ttl.migrations")
+                .build();
+
+        Ox ox = Ox.setUp(config);
+        ox.up();
+
+        List<Document> allIndexes = getDefaultMongo()
+                .getDatabase(config.databaseName())
+                .getCollection("ttl_collection")
+                .listIndexes().into(new ArrayList<>())
+                .stream().filter(d -> !d.getString("name").equals("_id_")).toList();
+
+        Assertions.assertThatCollection(allIndexes)
+                .extracting("name")
+                .containsExactlyInAnyOrder("ttl_index");
+
+        Assertions.assertThat(allIndexes.get(0).get("expireAfterSeconds")).isEqualTo(600);
+
+    }
+}
---
+++ src/test/java/ox/integration/ttl/migrations/V0001__create_index.java
@@ -0,0 +1,24 @@
+package ox.integration.ttl.migrations;
+
+import ox.engine.exception.OxException;
+import ox.engine.internal.OxAction;
+import ox.engine.internal.OxEnvironment;
+import ox.engine.structure.Migration;
+import ox.engine.structure.OrderingType;
+
+public class V0001__create_index implements Migration {
+
+    @Override
+    public void up(OxEnvironment env) throws OxException {
+        OxAction.createIndex("ttl_index")
+                .addAttribute("createdAt", OrderingType.ASC)
+                .setCollection("ttl_collection")
+                .markAsTTL(600)
+                .execute(env);
+    }
+
+    @Override
+    public void down(OxEnvironment env) throws OxException {
+
+    }
+}
